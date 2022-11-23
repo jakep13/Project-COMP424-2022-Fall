2b2b25fa0184ad27abd5d74c07e1b240fe9d0be6
jakep13 <jake.pogharian@mail.mcgill.ca>
2022-11-22 20:15:36 -0500

    started student agent.py

--- agents/student_agent.py
+++ agents/student_agent.py
@@ -1,8 +1,11 @@
 # Student agent: Add your own agent here
+from logging import root
 from agents.agent import Agent
 from store import register_agent
 import sys
-
+import numpy as np
+# import math #MUST ADD THIS TO REQUIREMENTS.TXT!!!
+EXP_PARAM = 2
 
 @register_agent("student_agent")
 class StudentAgent(Agent):
@@ -20,6 +23,60 @@ class StudentAgent(Agent):
             "d": 2,
             "l": 3,
         }
+
+        # define Monte Carlo Tree Searc
+    class MCTS:
+        pass
+        # define a node of the Monte Carlo Tree
+        class Node:
+            def __init__(self, board, move, parent, evaluator):
+                self.board = board
+                self.move = move
+                self.parent = parent
+                self.visits = 0
+                self.wins = 0
+                self.children = []
+                self.evaluator = evaluator
+
+            def addVisit(self):
+               self. visit+=1
+            
+            def addWin(self, w):
+                self.wins+=w
+
+            def getRandomChild(self):
+                if not self.children:
+                    return None
+                return self.children[np.random.randint(0, len(self.children))]
+            
+            def getMaxChild(self):
+                return max(self.children, key=self.evaluator)
+        
+        def __init__(self, board):
+            self.root = self.Node(board, move = None, parent = None, evaluator = self.uct_evaluator)
+    
+
+        def uct_evaluator(node):
+            if( node.visits == 0): return 1000000000
+            return (node.wins / node.visits) + EXP_PARAM * np.sqrt(np.log(node.parent.visits)/node.visits)
+
+        def choose_move(board):
+            pass
+
+        
+
+        
+
+
+
+            
+
+            
+
+
+
+    
+
 
     def step(self, chess_board, my_pos, adv_pos, max_step):
         """
